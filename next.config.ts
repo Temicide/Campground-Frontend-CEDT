3c35ddfde5b7864ee28837d0cf659dd787b48a3e
Athiphat Saramas <athiphatsaramas@gmail.com>
2026-03-24 23:08:41 +0700

    Connected between campgrounds page and api /campgrounds

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        // Proxy to the external Next.js backend to bypass CORS
+        destination: "https://campground-backend-cedt.vercel.app/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
